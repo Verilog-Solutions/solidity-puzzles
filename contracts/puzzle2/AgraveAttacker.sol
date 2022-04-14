d80feed120936b391a0f2673a56133484151c26f
RuiXi-Verilog <rui.x@verilog.solutions>
2022-04-13 17:09:37 -0700

    Add logo char art to files

--- contracts/puzzle2/AgraveAttacker.sol
+++ contracts/puzzle2/AgraveAttacker.sol
@@ -1,4 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0
+/**  Do NOT use this code in production **
+    _    _           _ _             
+   | |  | |         (_) |            
+   | |  | |___  ____ _| | ___   ____ 
+    \ \/ / _  )/ ___) | |/ _ \ / _  |
+     \  ( (/ /| |   | | | |_| ( ( | |
+    _ \/ \____)_|   |_|_|\___/ \_|| |
+   | |        | |      _      (_____|
+    \ \   ___ | |_   _| |_ (_) ___  ____   ___ 
+     \ \ / _ \| | | | |  _)| |/ _ \|  _ \ /___)
+ _____) ) |_| | | |_| | |__| | |_| | | | |___ |
+(______/ \___/|_|\____|\___)_|\___/|_| |_(___/ 
+                                               
+Verilog Solutions Inc. https://www.verilog.solutions
+Verilog is a full-stack web3 security firm, covering smart contract auditing, 
+validator opertions, venture investment, and incubation.
+Glad to prepare and present the materials for Game Day Remix in DevConnect Amsterdam!
+*/
+
 pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC777/IERC777Recipient.sol";
 import "./interface/IAgraveVictim.sol";
--- test/puzzle1.test.js
+++ test/puzzle1.test.js
@@ -1,3 +1,22 @@
+/**  Do NOT use this code in production **
+    _    _           _ _             
+   | |  | |         (_) |            
+   | |  | |___  ____ _| | ___   ____ 
+    \ \/ / _  )/ ___) | |/ _ \ / _  |
+     \  ( (/ /| |   | | | |_| ( ( | |
+    _ \/ \____)_|   |_|_|\___/ \_|| |
+   | |        | |      _      (_____|
+    \ \   ___ | |_   _| |_ (_) ___  ____   ___ 
+     \ \ / _ \| | | | |  _)| |/ _ \|  _ \ /___)
+ _____) ) |_| | | |_| | |__| | |_| | | | |___ |
+(______/ \___/|_|\____|\___)_|\___/|_| |_(___/ 
+                                               
+Verilog Solutions Inc. https://www.verilog.solutions
+Verilog is a full-stack web3 security firm, covering smart contract auditing, 
+validator opertions, venture investment, and incubation.
+Glad to prepare and present the materials for Game Day Remix in DevConnect Amsterdam!
+*/
+
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
